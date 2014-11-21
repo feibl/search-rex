6db28c491d7a71fe8f041cb98fe0d87167bfaa42
Fabian Senn <fabian.senn@gmail.com>
2014-11-21 11:28:54 +0100

    testing with new relevance parameter structure

--- tests/test_relevance.py
+++ tests/test_relevance.py
@@ -23,16 +23,16 @@ def test__compute_relevance__record_not_present():
 
 
 def test__compute_weighted_relevance__record_present():
-    query_hit_rows = [
-        {'doc1': 4, 'doc2': 2, 'doc3': 1, 'doc4': 3},
-        {'doc3': 3, 'doc5': 7},
-        {'doc1': 1, 'doc2': 5, 'doc5': 4},
-    ]
-    query_sims = [
-        0.25,
-        0.1,
-        0.75,
-    ]
+    query_hit_rows = {
+        'query_1': {'doc1': 4, 'doc2': 2, 'doc3': 1, 'doc4': 3},
+        'query_2': {'doc3': 3, 'doc5': 7},
+        'query_3': {'doc1': 1, 'doc2': 5, 'doc5': 4},
+    }
+    query_sims = {
+        'query_1': 0.25,
+        'query_2': 0.1,
+        'query_3': 0.75,
+    }
     w_rel_doc1 = compute_w_relevance('doc1', query_hit_rows, query_sims)
     w_rel_doc2 = compute_w_relevance('doc2', query_hit_rows, query_sims)
     assert abs(w_rel_doc1 - 0.175) <= 0.00001
@@ -40,15 +40,15 @@ def test__compute_weighted_relevance__record_present():
 
 
 def test__compute_weighted_relevance__record_not_present():
-    query_hit_rows = [
-        {'doc1': 4, 'doc2': 2, 'doc3': 1, 'doc4': 3},
-        {'doc3': 3, 'doc5': 7},
-        {'doc1': 1, 'doc2': 5, 'doc5': 4},
-    ]
-    query_sims = [
-        0.25,
-        0.1,
-        0.75,
-    ]
+    query_hit_rows = {
+        'query_1': {'doc1': 4, 'doc2': 2, 'doc3': 1, 'doc4': 3},
+        'query_2': {'doc3': 3, 'doc5': 7},
+        'query_3': {'doc1': 1, 'doc2': 5, 'doc5': 4},
+    }
+    query_sims = {
+        'query_1': 0.25,
+        'query_2': 0.1,
+        'query_3': 0.75,
+    }
     w_rel_doc6 = compute_w_relevance('doc6', query_hit_rows, query_sims)
     assert w_rel_doc6 == 0.0

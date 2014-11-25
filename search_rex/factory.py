81a57842672d46c25b702766e4bb6a18183fe54f
Fabian Senn <fabian.senn@gmail.com>
2014-11-25 11:29:59 +0100

    factory imports models

--- search_rex/factory.py
+++ search_rex/factory.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from core import db
+from .core import db
+from .models import *
 
 
 def create_app(config_path=None):
@@ -7,10 +8,10 @@ def create_app(config_path=None):
     app.config.from_object(
         config_path if config_path else 'config.DevelopmentConfig')
 
+    db.init_app(app)
+
     from api.recommender import rec_api
 
     app.register_blueprint(rec_api)
-
-    db.init_app(app)
 
     return app

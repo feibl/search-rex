4f25d4bd1e07939e897cf4b5fffc85d7f8ceb962
Fabian Senn <fabian.senn@gmail.com>
2015-01-19 09:21:35 +0100

    Function for recommending records that are most similar to a target record implemented

--- search_rex/recommendations/recommenders/item_based.py
+++ search_rex/recommendations/recommenders/item_based.py
@@ -35,6 +35,9 @@ class RecordBasedRecommender(AbstractRecordBasedRecommender):
         self.record_sim = record_sim
 
     def recommend(self, session_id, max_num_recs=10):
+        """
+        Recommends records based on the recent actions in the session
+        """
         candidates = defaultdict(float)
         seen_records = list(self.data_model.get_seen_records(session_id))
         print('Seen records by {}: {}'.format(session_id, seen_records))
@@ -50,6 +53,23 @@ class RecordBasedRecommender(AbstractRecordBasedRecommender):
 
         candidates_by_score = sorted(
             candidates.items(), key=lambda (p_id, sim): sim,
+            reverse=True)
+
+        return candidates_by_score[:max_num_recs]\
+            if max_num_recs is not None else candidates_by_score
+
+    def most_similar_records(self, record_id, max_num_recs=10):
+        """
+        Retrieves records that are most similar to the given record
+        """
+        candidates = []
+        for nbour in self.record_nhood.get_neighbours(record_id):
+            similarity = self.record_sim.get_similarity(record_id, nbour)
+            if not math.isnan(similarity):
+                candidates.append((nbour, similarity))
+
+        candidates_by_score = sorted(
+            candidates, key=lambda (p_id, sim): sim,
             reverse=True)
 
         return candidates_by_score[:max_num_recs]\
